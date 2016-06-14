524f51affb83f466cba58c1129dc52105a7caac9
Steve Pentler <stevepentler@gmail.com>
2016-06-14 15:58:50 -0600

    Orders by active status

--- app/controllers/api/v1/surveys_controller.rb
+++ app/controllers/api/v1/surveys_controller.rb
@@ -2,12 +2,16 @@ class Api::V1::SurveysController < ApplicationController
   respond_to :json
 
   def index
-    respond_with current_user.surveys
+    respond_with current_user.surveys.order(active: :desc)
   end
 
   def create
     create_survey_with_options(params)
     render json: "success"
   end
+
+  # def survey_params
+  #   params.require(:survey).permit(:surveyName, :surveyQuestion, :surveyExpiration, :surveyOptions)
+  # end
 
 end
