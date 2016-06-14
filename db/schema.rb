ef5c9da47a97bea47a0a2b10dea5df3f8275b57a
Steve Pentler <stevepentler@gmail.com>
2016-06-14 15:35:02 -0600

    Defaults active to true for new survey

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614193311) do
+ActiveRecord::Schema.define(version: 20160614213325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20160614193311) do
   create_table "surveys", force: :cascade do |t|
     t.integer "user_id"
     t.string  "name"
-    t.boolean "active"
+    t.boolean "active",     default: true
     t.string  "question"
     t.integer "expiration"
   end
