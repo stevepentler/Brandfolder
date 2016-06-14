ca64145013c65cb50e4553e81ba9ecfc8b73e1c3
Steve Pentler <stevepentler@gmail.com>
2016-06-14 14:12:49 -0600

    Displays active/inactive survey

--- app/assets/javascripts/components/_survey.js.jsx
+++ app/assets/javascripts/components/_survey.js.jsx
@@ -3,11 +3,12 @@
 const Survey = React.createClass({
   render() {
     let survey = this.props.survey;
-    let path = `surveys/${survey.id}`
+    let path = `surveys/${survey.id}`;
+    let status = survey.active ? "Active" : "Inactive";
     console.log("survey", survey)
     return(
       <a href={path}>
-        <h3>{survey.name}</h3>
+        <h3>{survey.name} - {status}</h3>
       </a>
     )
   }
