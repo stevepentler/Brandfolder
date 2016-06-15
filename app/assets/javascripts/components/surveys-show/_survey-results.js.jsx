6de7a1763454b98cde06fefccb09d9d77267ba05
Steve Pentler <stevepentler@gmail.com>
2016-06-15 12:51:32 -0600

    Tentative sharing on facebook and twitter, will utilize direct href in production

--- app/assets/javascripts/components/surveys-show/_survey-results.js.jsx
+++ app/assets/javascripts/components/surveys-show/_survey-results.js.jsx
@@ -5,16 +5,23 @@ const SurveyResults = React.createClass({
     let survey = this.props.survey;
     let votes = this.props.votes;
     let link = `/surveys/${survey.id}`
+    let facebookLink = <div class="fb-share-button" data-href={link} data-layout="button_count" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fgithub.com%2Fstevepentler&amp;src=sdkpreparse">Share on Facebook</a></div>
+    let twitterLink = <a href="https://twitter.com/share" class="twitter-share-button" data-url={link} data-via="Pentophile">Tweet</a>
+
     return(
       <div>
         <h3>{survey.name}</h3>
         <a href={link}>
           <span>sharable link</span>
         </a>
-<div class="fb-share-button" data-href="https://127.0.0.1:3000/surveys/1" data-layout="button_count" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Flocalhost%3A3000%2Fsurveys%2F1&amp;src=sdkpreparse">Share</a></div>
+        {facebookLink}
+        {twitterLink}
         < Options options={this.props.options}
                   survey={this.props.survey}
                   handleVote={this.props.handleVote} />
+        < Graph   options={this.props.options}
+                  survey={this.props.survey}
+                  votes={this.props.votes} />
       </div>
     )
   }
