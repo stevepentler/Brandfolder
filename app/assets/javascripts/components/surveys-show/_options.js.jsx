0195a51571ce3e848ec06f5656091e2675988a4e
Steve Pentler <stevepentler@gmail.com>
2016-06-15 11:29:23 -0600

    Conditionally renders options based on survey state

--- app/assets/javascripts/components/surveys-show/_options.js.jsx
+++ app/assets/javascripts/components/surveys-show/_options.js.jsx
@@ -4,13 +4,18 @@ const Options = React.createClass({
   listOptions(option) {
     return (
       <div key={option.id}>
-        <button onClick={this.props.handleVote.bind(this, option)} className="btn">Vote for: {option.title}</button>
+        <button onClick={this.props.handleVote.bind(null, option)} className="btn">Vote for: {option.title}</button>
       </div>
     )
   },
 
   render() {
-    let options = this.props.options.map(this.listOptions);
+    let options;
+    if (this.props.survey.active) {
+      options = this.props.options.map(this.listOptions);
+    } else {
+      options = <h4>This survey has closed!</h4>;
+    }
     return (
       <div>
         {options}
--- app/assets/javascripts/components/surveys-show/_survey-results.js.jsx
+++ app/assets/javascripts/components/surveys-show/_survey-results.js.jsx
@@ -13,6 +13,7 @@ const SurveyResults = React.createClass({
         </a>
 <div class="fb-share-button" data-href="https://127.0.0.1:3000/surveys/1" data-layout="button_count" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Flocalhost%3A3000%2Fsurveys%2F1&amp;src=sdkpreparse">Share</a></div>
         < Options options={this.props.options}
+                  survey={this.props.survey}
                   handleVote={this.props.handleVote} />
       </div>
     )
